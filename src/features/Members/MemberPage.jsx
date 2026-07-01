06bc0c038568eae9ada74955643712718da98b30
abdoKhaldi <khaldiabdenabi@gmail.com>
2026-07-01 05:27:46 +0100

    implement common stateCard in MemberPage

--- src/features/Members/MemberPage.jsx
+++ src/features/Members/MemberPage.jsx
@@ -1,4 +1,5 @@
 import { useState, useMemo, useCallback } from "react";
+import StateCard from '../../features/commonCards/StateCard';
 import {
   HiOutlineSearch,
   HiOutlineChevronUp,
@@ -214,39 +215,6 @@ function SortIcon({ columnKey, sortConfig }) {
   return <span className="ml-1 text-gray-300">⇅</span>;
 }
 
-function StatCard({ label, value, icon, color }) {
-  const colorMap = {
-    green: "bg-white text-green-800 border-white shadow-2xs",
-    emerald: "bg-white text-green-800 border-white shadow-2xs",
-    gray: "bg-white text-green-800 border-white shadow-2xs",
-    teal: "bg-white text-green-800 border-white shadow-2xs",
-  };
-  const iconBgMap = {
-    green: " bg-blue-300 text-white",
-    emerald: "bg-green-300 text-white",
-    gray: "bg-red-500 text-white",
-    teal: "bg-yellow-300 text-white",
-  };
-
-  return (
-    <div
-      className={`flex items-start justify-between  gap-3 rounded-xl border p-4 ${colorMap[color] || colorMap.green}`}
-    >
-      <div className="flex-1 flex-col items-center justify-between">
-        
-        <p className="font-bold text-gray-300">{label.toUpperCase()}</p>
-        <p className="text-3xl font-bold">{value?value:0}</p>
-      </div>
-      
-      <div
-        className={`flex h-10 w-10 shrink-0 items-center justify-center rounded-lg ${iconBgMap[color] || iconBgMap.green}`}
-      >
-        {icon}
-      </div>
-
-    </div>
-  );
-}
 
 function StatusBadge({ status }) {
   if (status === "active") {
@@ -532,27 +500,27 @@ export default function MemberPage() {
 
         {/* ── Stats Cards ────────────────────────────────────────────────── */}
         <div className="grid grid-cols-2 gap-4 sm:grid-cols-4 ">
-          <StatCard
+          <StateCard
             label="Total Members"
             value={stats.total}
             icon={<MdPeopleAlt className="h-5 w-5" />}
             color="green"
           />
-          <StatCard
+          <StateCard
             label="Active"
             value={stats.active}
             icon={<HiOutlineCheck className="h-5 w-5" />}
             color="emerald"
           />
-          <StatCard
+          <StateCard
             label="Inactive"
             value={stats.inactive}
             icon={<MdPersonOff className="h-5 w-5" />}
             color="gray"
           />
-          <StatCard
+          <StateCard
             label="Books Borrowed"
-            value={stats.totalBorgirowed}
+            value={stats.totalBorrowed}
             icon={<HiOutlineBookOpen className="h-5 w-5" />}
             color="teal"
           />
